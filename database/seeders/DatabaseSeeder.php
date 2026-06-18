43369801feb7779d72285e12cbdcd36b2bcc3232
josueqalvarez <quichcajosue@gmail.com>
2026-06-17 20:19:06 -0500

    Ingreso de datos aleatorios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Question;
+use App\Models\Answer;
+use App\Models\Comment;
+use App\Models\Blog;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +20,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(19)->create();
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'i@test.com',
         ]);
+    
+        $categories = Category::factory(4)->create();
+
+        $questions = Question::factory(30)->create(
+            [
+                'category_id' => fn() => $categories->random()->id,
+                'user_id' => fn() => User::inRandomOrder()->first()->id,
+            ]
+        );
+
+        $answers = Answer::factory(100)->create(
+            [
+                'question_id' => fn() => $questions->random()->id,
+                'user_id' => fn() => User::inRandomOrder()->first()->id,
+            ]
+        );
+
+        Comment::factory(100)->create(
+            [
+                'user_id' => fn() => User::inRandomOrder()->first()->id,
+                'commentable_id' => fn() => $answers->random()->id,
+                'commentable_type' => fn() => Answer::class,
+            ]
+        );
+
+        Blog::factory(10)->create(
+            [
+                'user_id' => fn() => User::inRandomOrder()->first()->id,
+                'category_id' => fn() => $categories->random()->id,
+            ]
+        );
+
+
+    
     }
 }
